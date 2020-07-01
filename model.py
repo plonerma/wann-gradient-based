a5e6bf4e8106d299dc441abcd7e7d54f5ab9578c
Max Ploner <git@maxploner.de>
2020-07-01 16:06:43 +0200

    Implemented @binary_connect-like STE for WANN-like network

--- model.py
+++ model.py
@@ -1,11 +1,32 @@
+"""This module implements a WANN-like network and straight through estimation
+(STE, see @binnary_connect).
+
+"""
+
+
 import torch
 from functools import reduce, partial
 import numpy as np
 
-class MultiActivationModule(torch.nn.Module):
-    """Applies multiple elementwise activation functions to a tensor."""
 
-    discretized = False
+class Discretizable:
+    def discretize_weight(self):
+        self.store_weight()
+        self.weight.data.copy_(self.effective_weight)
+
+    def store_weight(self):
+        self.stored_weight.data.copy_(self.weight.data)
+
+    def restore_weight(self):
+        self.weight.data.copy_(self.stored_weight.data)
+
+    def clip_weight(self):
+        torch.nn.functional.hardtanh(self.weight.data, inplace=True)
+
+
+
+class MultiActivationModule(torch.nn.Module, Discretizable):
+    """Applies multiple elementwise activation functions to a tensor."""
 
     available_act_functions = [
         ('relu', torch.relu),
@@ -13,28 +34,35 @@ class MultiActivationModule(torch.nn.Module):
         ('tanh', torch.tanh),
         ('gaussian (standard)', lambda x: torch.exp(-torch.square(x) / 2.0)),
         ('step', lambda t: (t > 0.0) * 1.0),
-        ('identity', lambda x: x),
-        ('inverse', torch.neg),
-        ('squared', torch.square),
-        ('abs', torch.abs),
-        ('cos', torch.cos),
-        ('sin', torch.sin),
+        #('identity', lambda x: x),
+        #('inverse', torch.neg),
+        #('squared', torch.square),
+        #('abs', torch.abs),
+        #('cos', torch.cos),
+        #('sin', torch.sin),
     ]
-
-    @property
-    def n_funcs(self):
-        return len(self.funcs)
 
     def __init__(self, n_out):
         super().__init__()
         self.funcs = [f[1] for f in self.available_act_functions]
 
         self.weight = torch.nn.Parameter(torch.zeros((self.n_funcs, n_out)))
-        self.frozen = torch.zeros(n_out, dtype=bool)
-        self.soft = torch.nn.Softmax(dim=0)
+        self.stored_weight = torch.empty_like(self.weight)
+
+    @property
+    def n_funcs(self):
+        return len(self.funcs)
+
+    def clip_weight(self):
+        self.weight.data = self.weight.data / torch.norm(self.weight.data, dim=0).unsqueeze(dim=0)
+
+    @property
+    def effective_weight(self):
+        indices = torch.max(self.weight, 0).indices
+        return torch.nn.functional.one_hot(indices, self.n_funcs).T.float()
 
     def forward(self, x):
-        coefficients = self.soft(self.weight)
+        coefficients = self.weight
 
         return reduce(
             lambda first, act: (
@@ -49,18 +77,25 @@ class MultiActivationModule(torch.nn.Module):
         )
 
 
-class RestrictedLinear(torch.nn.Module):
-    """Similar to torch.nn.Linear, but restricts weights between -1 and 1."""
+class TertiaryLinear(torch.nn.Module, Discretizable):
+    """Similar to torch.nn.Linear, with tertiary weights ($\in \{-1,0,1\}$)."""
 
     def __init__(self, n_in, n_out):
         super().__init__()
-        self.sign = torch.nn.Softsign()
-        self.weight = torch.nn.Parameter(torch.empty(n_in, n_out))
-        self.frozen = torch.zeros(*self.weight.size(), dtype=bool)
+
+        self.linear = torch.nn.Linear(n_in, n_out, bias=False)
+        self.stored_weight = torch.empty_like(self.weight)
+
+    @property
+    def weight(self):
+        return self.linear.weight
+
+    @property
+    def effective_weight(self):
+        return torch.sign(torch.nn.functional.hardshrink(self.weight, lambd=0.4))
 
     def forward(self, x):
-        weight = torch.where(self.frozen, self.weight, self.sign(self.weight) * 3/2)
-        return torch.nn.functional.linear(x, weight.T)
+        return self.linear(x)
 
 
 class ConcatLayer(torch.nn.Module):
@@ -68,7 +103,7 @@ class ConcatLayer(torch.nn.Module):
 
     def __init__(self, n_in, n_out, shared_weight):
         super().__init__()
-        self.linear = RestrictedLinear(n_in, n_out)
+        self.linear = TertiaryLinear(n_in, n_out)
         self.activation = MultiActivationModule(n_out)
 
         self.shared_weight = shared_weight
@@ -80,69 +115,36 @@ class ConcatLayer(torch.nn.Module):
 
         return torch.cat([x, inner_out], dim=-1)
 
+def restrict_weight(m):
+    if hasattr(m, 'discretize_weight'):
+        m.discretize_weight()
+
+def restore_weight(m):
+    if hasattr(m, 'restore_weight'):
+        m.restore_weight()
+
+def clip_weight(m):
+    if hasattr(m, 'clip_weight'):
+        m.clip_weight()
 
 def weight_init(m):
     """Initialize weights randomly."""
-    if isinstance(m, RestrictedLinear):
-        torch.nn.init.normal_(m.weight.data)
+    if isinstance(m, TertiaryLinear):
+        torch.nn.init.normal_(m.weight.data, std=.12)
     elif isinstance(m, MultiActivationModule):
-        torch.nn.init.normal_(m.weight.data)
-
-
-def unfreeze_parameters(m):
-    """Set all parameters to not frozen."""
-    if isinstance(m, MultiActivationModule):
-        m.frozen.fill_(False)
-    if isinstance(m, RestrictedLinear):
-        m.frozen.fill_(False)
-
-
-def reset_frozen_gradients(m):
-    """Set all gradients of frozen parameters to 0 (these parameters won't be updated by the optimizer)."""
-    if isinstance(m, MultiActivationModule):
-        m.weight.grad[:, m.frozen] = 0.0
-
-    if isinstance(m, RestrictedLinear):
-        m.weight.grad[m.frozen] = 0.0
-
-
-def freeze_some_act_funcs(m, ratio=0.2):
-    if isinstance(m, MultiActivationModule):
-        indices = torch.max(m.weight, 0).indices
-
-        to_freeze = (torch.rand(*m.frozen.size()) <= ratio) & ~m.frozen
-
-        if torch.any(to_freeze):
-            indices = torch.max(m.weight[:, to_freeze], 0).indices
-            m.weight.data[:, to_freeze] = torch.nn.functional.one_hot(indices, m.n_funcs).T.float()
-            m.frozen[to_freeze] = True
-
-
-def freeze_some_weights(m, ratio=0.2, zero_ratio=0.4):
-    if isinstance(m, RestrictedLinear):
-        # mask-0 operation
-        if torch.any(~m.frozen):
-            alpha_zero = np.percentile(m.weight.data[~m.frozen].abs(), 100 * ratio * zero_ratio)
-            mask_zero = (m.weight.data.abs() <= alpha_zero) & ~m.frozen
-
-            # mask-1 operation
-            alpha_one = np.percentile(-m.weight.data[~m.frozen].abs(), 100 * ratio * (1-zero_ratio))
-            mask_one = (-m.weight.data.abs() <= alpha_one) & ~m.frozen
-
-            m.frozen[mask_zero] = True
-            m.frozen[mask_one] = True
-
-            m.weight.data[mask_zero] = 0.0
-            m.weight.data[mask_one] = m.weight.data[mask_one].sign()
-
+        torch.nn.init.uniform_(m.weight.data, 0, 1)
 
 class Model(torch.nn.Module):
     def __init__(self, shared_weight, *layer_sizes):
         super().__init__()
 
+        self.layer_sizes = layer_sizes
+
         layers = list()
 
         n_in = layer_sizes[0]
+
+        self.shared_weight = shared_weight
 
         for n_out in layer_sizes[1:]:
             layers.append(ConcatLayer(n_in, n_out, shared_weight))
@@ -151,22 +153,40 @@ class Model(torch.nn.Module):
         self.network = torch.nn.Sequential(*layers)
         self.softmax = torch.nn.Softmax(dim=-1)
 
-    def forward(self, x):
-        net_out = self.network(x)
-        net_out = net_out[..., -3:]
-        return self.softmax(net_out)
+    def restrict(self):
+        self.apply(restrict_weight)
+
+    def restore(self):
+        self.apply(restore_weight)
+
+    def clip(self):
+        self.apply(clip_weight)
 
     def init_weights(self):
         self.apply(weight_init)
 
-    def reset_frozen_grads(self):
-        self.apply(reset_frozen_gradients)
+    @property
+    def n_in(self):
+        return self.layer_sizes[0]
 
-    def freeze_act_funcs(self, ratio=0.2):
-        self.apply(partial(freeze_some_act_funcs, ratio=ratio))
+    @property
+    def n_out(self):
+        return self.layer_sizes[-1]
 
-    def freeze_weights(self, ratio=0.2, zero_ratio=0.4):
-        self.apply(partial(freeze_some_weights, ratio=ratio, zero_ratio=zero_ratio))
+    def forward(self, x):
+        net_out = self.network(x)
+        net_out = net_out[..., -self.n_out:]
+        return self.softmax(net_out)
 
-    def unfreeze(self):
-        self.apply(unfreeze_parameters)
+
+def write_hist(writer, model, epoch):
+    effective_weights = list()
+    actual_weights = list()
+
+    for m in model.modules():
+        if isinstance(m, TertiaryLinear):
+            effective_weights.append(m.effective_weight.reshape(-1))
+            actual_weights.append(m.weight.reshape(-1))
+
+    writer.add_histogram("effective weights", torch.cat(effective_weights), epoch)
+    writer.add_histogram("actual weights", torch.cat(actual_weights), epoch)
--- tasks/iris.py
+++ tasks/iris.py
@@ -6,24 +6,12 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 
+def iris(test=False):
+    dataset = load_iris()
+    train_X, test_X, train_y, test_y = train_test_split(dataset['data'],
+                                                        dataset['target'], test_size=0.2)
 
-dataset = load_iris()
-#train_X, test_X, train_y, test_y = train_test_split(dataset['data'],
-#                                                    dataset['target'], test_size=0.2)
-
-
-# Just going for fitting here. (Todo: Change stuff in paper for usefull comparison)
-train_X = dataset['data']
-train_y = dataset['target']
-test_X = train_X
-test_y = train_y
-
-# add bias to the inputs
-train_X = np.hstack([train_X, np.ones((train_X.shape[0], 1))])
-test_X = np.hstack([test_X, np.ones((test_X.shape[0], 1))])
-
-# wrap up with Variable in pytorch
-train_X = Variable(torch.Tensor(train_X).float())
-test_X = Variable(torch.Tensor(test_X).float())
-train_y = Variable(torch.Tensor(train_y).long())
-test_y = Variable(torch.Tensor(test_y).long())
+    if not test:
+        return train_X, train_y
+    else:
+        return test_X, test_y
