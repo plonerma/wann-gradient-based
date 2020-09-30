45e8a3ab662cc99f93d4bbab098ab08c3e382d5c
Max Ploner <git@maxploner.de>
2020-09-30 15:37:09 +0200

    Minor changes

--- model.py
+++ model.py
@@ -263,7 +263,7 @@ class ConcatLayer(torch.nn.Module):
         # inputs are spread across dimension 1
         out_indices = torch.arange(self.out_features)
         return out_indices[(self.linear.weight.data.abs() < gamma).all(1)]
-    
+
     def recieves_input_from(self, gamma=0.4):
         return (self.linear.weight.data.abs() >= gamma).any(0)
 
@@ -354,22 +354,21 @@ class Model(torch.nn.Module):
         return grew
 
     def cleanup(self):
-        deleted_something = False
-    
         # delete nodes without input
         for i, layer in enumerate(self.hidden_layers):
             self.delete_nodes(layer.nodes_without_input(self.gamma) + layer.in_features)
-        
+
         # delete nodes that aren't used as inputs in later layers
         for i, layer in reversed(list(enumerate(self.hidden_layers))):
-            
+
             used_as_input = torch.zeros(layer.out_features, dtype=bool)
-            
+
             for later_layer in list(self.layers(hidden_only=False))[i+1:]:
                 used_as_input = used_as_input | later_layer.recieves_input_from()[layer.in_features:layer.in_features+layer.out_features]
 
-            self.delete_nodes(torch.arange(layer.out_features)[~used_as_input] + layer.in_features)
-            
+            self.delete_nodes(
+                torch.arange(layer.out_features)[~used_as_input] + layer.in_features)
+
         self.delete_empty_layers()
 
     def delete_empty_layers(self):
@@ -377,16 +376,16 @@ class Model(torch.nn.Module):
         for i in reversed(range(len(self.hidden_layers))):
             if self.hidden_layers[i].out_features == 0:
                 del self.hidden_layers[i]
-    
+
     def delete_nodes(self, nodes):
         if nodes.shape[0] == 0:
             return
 
-        for layer in self.layers():    
+        for layer in self.layers():
             in_nodes = (nodes < layer.in_features)
             out_nodes = (nodes >= layer.in_features) & (nodes < (layer.in_features + layer.out_features))
             layer.delete_nodes_in(nodes[in_nodes])
-            layer.delete_nodes_out(nodes[out_nodes] - layer.in_features)  
+            layer.delete_nodes_out(nodes[out_nodes] - layer.in_features)
 
     def to_dict(self):
         return {
@@ -400,14 +399,14 @@ class Model(torch.nn.Module):
             n_in = state['hidden_layers.0.linear.weight'].size()[1]
         else:
             n_in = state['output_layer.linear.weight'].size()[1]
-        
+
         n_out = state['output_layer.linear.weight'].size()[0]
-        
+
         hidden_layer_sizes = list()
         for k, v in state.items():
             if k.startswith('hidden_layers.') and k.endswith('.linear.weight'):
                 hidden_layer_sizes.append(v.size()[0])
-                
+
         m = cls(shared_weight, n_in, n_out, hidden_layer_sizes=hidden_layer_sizes)
         m.load_state_dict(state)
         return m
